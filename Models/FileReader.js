e50def93dde74549577ac7e75a5d5ccf7a061c34
Jake Rossi <jakerossi24@gmail.com>
2018-02-03 10:37:43 +1100

    Ran Prettier on FileReader class and Table Class

--- Models/FileReader.js
+++ Models/FileReader.js
@@ -1,28 +1,28 @@
 const fs = require('fs'),
-  path = require('path'),
-  filePath = path.join(__dirname, '../commands.txt')
+	path = require('path'),
+	filePath = path.join(__dirname, '../commands.txt')
 
 module.exports = class FileReader {
-  constructor() {
-    this.file = null
-  }
+	constructor() {
+		this.file = null
+	}
 
-  loadFile(cb) {
-    fs.readFile(filePath, { encoding: 'utf-8' }, (err, commands) => {
-      if (!err) {
-        this.setData(commands)
-        cb()
-      } else {
-        throw Error(err)
-      }
-    })
-  }
+	loadFile(cb) {
+		fs.readFile(filePath, { encoding: 'utf-8' }, (err, commands) => {
+			if (!err) {
+				this.setData(commands)
+				cb()
+			} else {
+				throw Error(err)
+			}
+		})
+	}
 
-  setData(fileData) {
-    this.file = fileData.toString().trim()
-  }
+	setData(fileData) {
+		this.file = fileData.toString().trim()
+	}
 
-  getData() {
-    return this.file
-  }
+	getData() {
+		return this.file
+	}
 }
--- Models/Table.js
+++ Models/Table.js
@@ -1,37 +1,37 @@
 module.exports = class Table {
+	constructor() {
+		this.table = {}
+	}
 
-  constructor() {
-    this.table = {}
-  }
+	convertArrayToString(array) {
+		if (!(array instanceof Array))
+			throw Error('The parameter passed to convertArrayToString method must be a Array')
+		return array.toString()
+	}
 
-  convertArrayToString(array) {
-    if (!(array instanceof Array)) throw Error('The parameter passed to convertArrayToString method must be a Array')
-    return array.toString()
-  }
+	validParam(param) {
+		if (!Number.isInteger(param)) return false
+		return true
+	}
 
-  validParam(param) {
-    if (!(Number.isInteger(param))) return false
-    return true
-  }
+	throwIntError() {
+		throw Error('The parameter passed to validParam method must be a Integer')
+	}
 
-  throwIntError() {
-    throw Error('The parameter passed to validParam method must be a Integer')
-  }
+	createTable(width, height) {
+		if (width === undefined || !this.validParam(width)) this.throwIntError()
+		if (height === undefined || !this.validParam(height)) this.throwIntError()
 
-  createTable(width, height) {
-    if (width === undefined || !this.validParam(width)) this.throwIntError()
-    if (height === undefined || !this.validParam(height)) this.throwIntError()
+		Array.from(Array(width).keys()).forEach((column) => {
+			Array.from(Array(height).keys()).forEach((row) => {
+				let convertedArrayString = this.convertArrayToString([ column, row ])
 
-    Array.from(Array(width).keys()).forEach(column => {
-      Array.from(Array(height).keys()).forEach(row => {
-        let convertedArrayString = this.convertArrayToString([column, row])
+				this.table[convertedArrayString] = ''
+			})
+		})
+	}
 
-        this.table[convertedArrayString] = ''
-      })
-    })
-  }
-
-  getTable() {
-    return this.table
-  }
+	getTable() {
+		return this.table
+	}
 }
