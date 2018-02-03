98e73b04dc689b89fc4bfafad2adf4e4a1517319
Jake Rossi <jakerossi24@gmail.com>
2018-02-04 00:01:20 +1100

    Refactor Table class due to overthinking the solution

--- Models/Table.js
+++ Models/Table.js
@@ -1,16 +1,9 @@
 module.exports = class Table {
 	constructor() {
-		this.table = {}
 		this.tableSize = {
 			width: null,
 			height: null,
 		}
-	}
-
-	convertArrayToString(array) {
-		if (!(array instanceof Array))
-			throw Error('The parameter passed to convertArrayToString method must be a Array')
-		return array.toString()
 	}
 
 	validParam(param) {
@@ -30,14 +23,6 @@ module.exports = class Table {
 			width,
 			height,
 		}
-
-		Array.from(Array(width).keys()).forEach((column) => {
-			Array.from(Array(height).keys()).forEach((row) => {
-				let convertedArrayString = this.convertArrayToString([ column, row ])
-
-				this.table[convertedArrayString] = ''
-			})
-		})
 	}
 
 	getTable() {
@@ -46,15 +31,5 @@ module.exports = class Table {
 
 	getTableSize() {
 		return this.tableSize
-	}
-
-	removePosition(position) {
-		this.setPosition({ ...position, direction: '' })
-	}
-
-	setPosition(position) {
-		const { x, y, direction } = position
-		const convertedArrayString = this.convertArrayToString([ x, y ])
-		this.table[convertedArrayString] = direction
 	}
 }
